cd688dddc80f4234e364637364d544650765e179
benamiller <mrbenamiller@gmail.com>
2025-04-10 17:10:26 +0100

    Need to read lat and long from input file

---
+++ cs412_idm/assignments/a3/main.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def read_data(filename):
+    points = []
+    try:
+        with open(filename, 'r') as f:
+            for line in f:
+                try:
+                    lon_str, lat_str = line.strip().split(',')
+                    points.append([float(lon_str), float(lat_str)])
+                except ValueError:
+                    print(f"Sus value, skipping line: {line.strip()}")
+    except FileNotFoundError:
+        print(f"File not found, {filename}")
+        raise
+    except Exception as e:
+        print(f"Error occurred reading {filename}, {e}")
+        raise
+
+    return np.array(points)
+
+if __name__ == "__main__":
+    print(read_data("places.txt"))
