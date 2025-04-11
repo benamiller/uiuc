e8fded26fa46505ffaea76c81c8add006e2b5d4f
benamiller <mrbenamiller@gmail.com>
2025-04-11 12:51:12 +0100

    Long line

--- cs412_idm/assignments/a4/template/python/submission.py
+++ cs412_idm/assignments/a4/template/python/submission.py
@@ -11,7 +11,7 @@ class Solution:
           - X: 2D input data
           - K: the number of output clusters
         Returns:
-          A list of integers (range from 0 to K - 1) that represent class labels.
+          A list of integers (from 0 to K - 1) that represent class labels.
           The number does not matter as long as the clusters are correct.
           For example: [0, 0, 1] is treated the same as [1, 1, 0]"""
         # implement this function
