6b2d0a348cea17337140877da46249b3a76caaba
benamiller <mrbenamiller@gmail.com>
2025-05-22 13:01:00 +0100

    Adds fun1 tail recursion implementation

--- cs421_pl/tail.hs
+++ cs421_pl/tail.hs
@@ -7,3 +7,8 @@ fun2 n = 1 + fun2 (n `div` 2)
 fun3 1 = 1
 fun3 2 = 1
 fun3 n = fun3 (n-1) + fun3 (n-2)
+
+fun1_tail xx = aux xx 0
+  where
+    aux [] a = a
+    aux (x:xs) a | even x = aux xs (a-1) | odd x = aux xs (a+1)
