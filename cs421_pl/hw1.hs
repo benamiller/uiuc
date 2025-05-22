8d4bc5edcdf2948258204c83a434a0a63caf4ab1
benamiller <mrbenamiller@gmail.com>
2025-05-22 13:38:03 +0100

    Adds 1.4 solution

--- cs421_pl/hw1.hs
+++ cs421_pl/hw1.hs
@@ -11,4 +11,9 @@ power x 0 = 1
 power x y = x * power x (y-1)
 
 tailPower :: Integer -> Integer -> Integer
-tailPower = undefined
+
+tailPower x y = aux x y 1
+  where
+    aux x 0 a = a
+    aux x y a = aux x (y-1) (a*x)
+
