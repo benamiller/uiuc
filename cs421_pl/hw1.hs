71ed4fbf9be64bb0d14065b852543b3a3622b79a
benamiller <mrbenamiller@gmail.com>
2025-05-22 13:31:40 +0100

    Adds 1.4 start

--- cs421_pl/hw1.hs
+++ cs421_pl/hw1.hs
@@ -9,3 +9,6 @@ sumList (x:xs) = x + sumList xs
 power :: Integer -> Integer -> Integer
 power x 0 = 1
 power x y = x * power x (y-1)
+
+tailPower :: Integer -> Integer -> Integer
+tailPower = undefined
