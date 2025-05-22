7aeddd126ee149914ce6715ace41074230e56bf6
benamiller <mrbenamiller@gmail.com>
2025-05-22 11:24:24 +0100

    Implements activity 1

---
+++ cs421_pl/activity_1.hs
@@ -0,0 +1,21 @@
+inc :: Integer -> Integer
+inc a = a + 1
+
+
+fibValue = fib 6
+	where
+		fib 0 = 0
+		fib 1 = 1
+		fib 2 = 1
+		fib 3 = 2
+		fib n = fib(n - 1) + fib(n - 2)
+
+
+summedList = sumList [1,2,3]
+	where
+		sumList x = sum (x)
+
+
+incedList = incList [1,2,3]
+	where
+		incList l = map inc (l)
