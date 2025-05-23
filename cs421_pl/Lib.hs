53a623de0fd8bfc0795350e6e8d8a1f2fdf0552a
benamiller <mrbenamiller@gmail.com>
2025-05-23 17:13:01 +0100

    Initial 4 solved

---
+++ cs421_pl/Lib.hs
@@ -0,0 +1,131 @@
+--- Getting Started
+--- ===============
+
+--- Relevant Files
+--- --------------
+
+module Lib where
+
+-- This line imports the Prelude module without certain functions
+import Prelude hiding ( take, drop, reverse
+                      , zip, zipWith
+                      , map, foldl, foldr
+                      , iterate, repeat
+                      , replicate, cycle
+                      , (++)
+                      )
+-- When you are allowed to use builtin functions Prepend them with "P."
+-- for example `P.take`
+import qualified Prelude as P
+
+--- Problems
+--- ========
+
+--- Recursion
+--- ---------
+
+--- ### mytake
+
+-- don't forget to put the type declaration or you will lose points!
+-- mytake :: Int -> [a] -> [a]
+-- mytake 0 xs = xs
+-- mytake n (x:xs) = mytake (n-1) xs
+mytake :: Int -> [a] -> [a]
+mytake n _  | n <= 0 = []
+mytake _ [] = []
+mytake n (x:xs) = x : mytake (n-1) xs
+
+--- ### mydrop
+
+-- don't forget to put the type declaration or you will lose points!
+mydrop :: Int -> [a] -> [a]
+mydrop n xs | n <= 0 = xs
+mydrop _ [] = []
+mydrop n (x:xs) = mydrop (n-1) xs
+
+--- ### rev
+
+-- don't forget to put the type declaration or you will lose points!
+rev :: [a] -> [a]
+rev xs = aux xs []
+  where
+    aux [] acc = acc
+    aux (x:xs) acc = aux xs (x:acc)
+
+--- ### app
+
+-- don't forget to put the type declaration or you will lose points!
+app :: [a] -> [a] -> [a]
+app [] b = b
+app (a:as) b = a : app as b
+
+--- ### inclist
+
+-- don't forget to put the type declaration or you will lose points!
+inclist = undefined
+
+--- ### sumlist
+
+-- don't forget to put the type declaration or you will lose points!
+sumlist = undefined
+
+--- ### myzip
+
+-- don't forget to put the type declaration or you will lose points!
+myzip = undefined
+
+--- ### addpairs
+
+-- don't forget to put the type declaration or you will lose points!
+addpairs = undefined
+
+--- ### ones
+
+-- don't forget to put the type declaration or you will lose points!
+ones = undefined
+
+--- ### nats
+
+-- don't forget to put the type declaration or you will lose points!
+nats = undefined
+
+--- ### fib
+
+-- don't forget to put the type declaration or you will lose points!
+fib = undefined
+
+--- Set Theory
+--- ----------
+
+--- ### add
+
+-- don't forget to put the type declaration or you will lose points!
+add = undefined
+
+--- ### union
+
+-- don't forget to put the type declaration or you will lose points!
+union = undefined
+
+--- ### intersect
+
+-- don't forget to put the type declaration or you will lose points!
+intersect = undefined
+
+--- ### powerset
+
+-- don't forget to put the type declaration or you will lose points!
+powerset = undefined
+
+--- Higher Order Functions
+--- ----------------------
+
+--- ### inclist'
+
+-- don't forget to put the type declaration or you will lose points!
+inclist' = undefined
+
+--- ### sumlist'
+
+-- don't forget to put the type declaration or you will lose points!
+sumlist' = undefined
