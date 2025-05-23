f1725fc28db846c7f96103c3b96a7ae1f99ec3dc
benamiller <mrbenamiller@gmail.com>
2025-05-23 09:34:42 +0100

    Adds twice higher ord er function example

--- cs421_pl/w1_experiments.hs
+++ cs421_pl/w1_experiments.hs
@@ -11,9 +11,16 @@ compose f g x = f (g x)
 doubleValue = double 10
 composeValue = compose inc double 10
 
+twice f x = f (f x)
+
+twiceValue = twice inc 5
+twiceValue2 = twice twice inc 4
+
 main :: IO ()
 main = do
   print sqrValue
   print hypotsqValue
   print doubleValue
   print composeValue
+  print twiceValue
+  print twiceValue2
