832a51827996ae57267a78468f29974ca7819ac4
benamiller <mrbenamiller@gmail.com>
2025-05-23 09:31:49 +0100

    Adds more higher order function examples

--- cs421_pl/w1_experiments.hs
+++ cs421_pl/w1_experiments.hs
@@ -4,7 +4,16 @@ hypotsq a b = sqr a + sqr b
 sqrValue = sqr 10
 hypotsqValue = hypotsq 3 4
 
+inc x = x + 1
+double x = x * 2
+compose f g x = f (g x)
+
+doubleValue = double 10
+composeValue = compose inc double 10
+
 main :: IO ()
 main = do
   print sqrValue
   print hypotsqValue
+  print doubleValue
+  print composeValue
