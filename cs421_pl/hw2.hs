8715f3f1f129a9e1e5649f4dfeee62b852545dd3
benamiller <mrbenamiller@gmail.com>
2025-05-26 15:20:41 +0100

    Turtles all the way down

--- cs421_pl/hw2.hs
+++ cs421_pl/hw2.hs
@@ -14,3 +14,20 @@ palindromeCheckHelp string = string == reverse string
 declist :: Num a => [a] -> [a]
 declist [] = []
 declist list = map (\x -> x - 1) list
+
+data Turtle a = Down (Turtle a) (Turtle a) | Up a
+  deriving show
+
+listifyTurtle :: Turtle a -> [a]
+listifyTurtle (Up val) = [val]
+listifyTurtle (Down left right) = listifyTurtle left ++ listifyTurtle right
+
+isBalanced :: Turtle a -> Bool
+isBalanced (Up _) = True
+isBalanced (Down left right) =
+  let balanced = isBalanced left && isBalanced right
+    sameType = case (left, right) of
+      (Up _, Up _) -> True
+      (Down _ _, Down _ _) -> True
+      _ -> False
+  in balanced && sameType
