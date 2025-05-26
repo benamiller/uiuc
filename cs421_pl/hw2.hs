9d12003b74a5f18e4191dc47fde891e0cd098c4c
benamiller <mrbenamiller@gmail.com>
2025-05-26 14:43:56 +0100

    Adds foldl implementation

--- cs421_pl/hw2.hs
+++ cs421_pl/hw2.hs
@@ -1,3 +1,7 @@
 mymap :: (a -> b) -> [a] -> [b]
 mymap f [] = []
 mymap f (a:as) = f a : mymap f as
+
+myfoldl :: (b -> a -> b) -> b -> [a] -> b
+myfoldl f x [y] = f x y
+myfoldl f x (y:ys) = myfoldl f (f x y) ys
