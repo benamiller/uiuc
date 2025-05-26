410caaaa16e54912f38e629f09d1ea9a94f3bb0f
benamiller <mrbenamiller@gmail.com>
2025-05-26 14:59:00 +0100

    Adds palindrome check map solution

--- cs421_pl/hw2.hs
+++ cs421_pl/hw2.hs
@@ -5,3 +5,8 @@ mymap f (a:as) = f a : mymap f as
 myfoldl :: (b -> a -> b) -> b -> [a] -> b
 myfoldl f x [] = x
 myfoldl f x (y:ys) = myfoldl f (f x y) ys
+
+palindromeCheck :: [String] -> [Bool]
+palindromeCheck [] = []
+palindromeCheck list = map palindromeCheckHelp list
+palindromeCheckHelp string = string == reverse string
