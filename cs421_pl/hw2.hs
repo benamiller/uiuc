a004738f20f64c42e2a850eae178f46cbcffb6cf
benamiller <mrbenamiller@gmail.com>
2025-05-26 14:53:00 +0100

    Needed to address empty list case

--- cs421_pl/hw2.hs
+++ cs421_pl/hw2.hs
@@ -3,5 +3,5 @@ mymap f [] = []
 mymap f (a:as) = f a : mymap f as
 
 myfoldl :: (b -> a -> b) -> b -> [a] -> b
-myfoldl f x [y] = f x y
+myfoldl f x [] = x
 myfoldl f x (y:ys) = myfoldl f (f x y) ys
